189b8697bc29c5234b212c0fae7ae064857f4dc4
nakamura <nkc.daichi.nakamura@gmail.com>
2023-08-04 16:17:22 +0900

    スポナーとか実装しました

--- Assets/Scripts/Ingame/Customer/Customer.cs
+++ Assets/Scripts/Ingame/Customer/Customer.cs
@@ -7,25 +7,72 @@ using System.Linq;
 
 namespace Game
 {
+    public struct Order : IEquatable<Order>
+    {
+        public Order(Food food, int orderNumber)
+        {
+            Food = food;
+            IsProvided = false;
+            _orderNumber = orderNumber + 1;
+        }
+
+        public Food Food;
+        public bool IsProvided;
+
+        public int OrderNumber
+        {
+            get
+            {
+                return _orderNumber - 1;
+            }
+        }
+        private int _orderNumber;
+
+        public override bool Equals(object obj)
+        {
+            return obj is Order order && Equals(order);
+        }
+
+        public bool Equals(Order other)
+        {
+            return EqualityComparer<Food>.Default.Equals(Food, other.Food) &&
+                   IsProvided == other.IsProvided &&
+                   _orderNumber == other._orderNumber;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Food, IsProvided, _orderNumber);
+        }
+    }
+
     public class Customer : MonoBehaviour, ICustomer
     {
-        private ReactiveDictionary<Food, bool> _requestFoodsDictionary = new ReactiveDictionary<Food, bool>();
         private Subject<Unit> _provideCompletedSubject = new Subject<Unit>();
-
-        public IReadOnlyReactiveDictionary<Food, bool> RequestFoods => _requestFoodsDictionary;
         public System.IObservable<Unit> ProvideCompleted => _provideCompletedSubject;
 
-        public List<Food> TestFoods;
+        private ReactiveCollection<Order> _orders = new ReactiveCollection<Order>();
+        public IReadOnlyReactiveCollection<Order> Orders => _orders;
+
+
+        [SerializeField]
+        [Range(1, 3)]
+        private int _requestCount = 1;
 
         public bool IsContains(Food food)
         {
-            // リクエストしていてかつまだ提供されてない
-            return _requestFoodsDictionary.TryGetValue(food, out var result) && !result;
+            var order = _orders.FirstOrDefault(order => order.Food == food && !order.IsProvided);
+
+            return order.OrderNumber != -1;
         }
 
         public void OnProvide(Food food)
         {
-            _requestFoodsDictionary[food] = true;
+            var order = _orders.FirstOrDefault(order => order.Food == food && !order.IsProvided);
+            if (order.OrderNumber == -1) return;
+
+            order.IsProvided = true;
+            _orders[order.OrderNumber] = order;
 
             if (IsComplete())
             {
@@ -36,17 +83,19 @@ namespace Game
 
         private bool IsComplete()
         {
-            return _requestFoodsDictionary.All(pair => pair.Value);
+            return _orders.All(order => order.IsProvided);
         }
 
         void Start()
         {
             // フードコンテナからランダムな料理を受け取ってくる
-            var foodContainer = ServiceLocator.Instance.Resolve<IFoodContainer>();
+            //var foodContainer = ServiceLocator.Instance.Resolve<IFoodContainer>();
+            var foodContainer = FindFirstObjectByType<FoodManager>();
 
-            foreach (var item in TestFoods)
+            for (int i = 0; i < _requestCount; i++)
             {
-                _requestFoodsDictionary.Add(item, false);
+                var food = foodContainer.GetRandomFood();
+                _orders.Add(new Order(food, i));
             }
         }
     }
---
+++ Assets/Scripts/Ingame/Customer/WIP/TestRandomProvider.cs
@@ -0,0 +1,22 @@
+using Game;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class TestRandomProvider : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            var customer = GetComponent<ICustomer>();
+            var foodManager = FindFirstObjectByType<FoodManager>();
+            var food = foodManager.GetRandomFood();
+            if (customer.IsContains(food))
+            {
+                customer.OnProvide(food);
+            }
+        }
+    }
+}
