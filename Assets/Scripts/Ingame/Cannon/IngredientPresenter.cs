66a24bbba0bc1318f90d3f6a6f6ad86bc36d6e52
tamaron <49875900+tamaron@users.noreply.github.com>
2023-08-04 15:34:08 +0900

    IngredientPresenterの実装

--- Assets/Scripts/Ingame/Cannon/IngredientPresenter.cs
+++ Assets/Scripts/Ingame/Cannon/IngredientPresenter.cs
@@ -15,10 +15,46 @@ namespace game
         
         void Start()
         {
+            // view to model
+            
+            // 材料がクリックされたとき
             _ingredientView.IngredientClicked.Subscribe(ingredient =>
             {
                 _ingredientSelector.AddIngredient(ingredient);
             }).AddTo(this);
+            
+            
+            
+            // model to view
+            
+            // 大砲に追加可能な材料の候補が変化したとき
+            _ingredientSelector.CandidateChanged.Subscribe(ingredients =>
+            {
+                AnimateIngredientShowHide(ingredients);
+            }).AddTo(this);
+
+            // 
+            _Cannon.FoodChanged.Subscribe(ingredients =>
+            {
+
+            }).AddTo(this);
+        }
+        
+        /// <summary>
+        /// ingredientsに含まれるUIをenable、そうでないものをdisableする
+        /// </summary>
+        /// <param name="ingredients"></param>
+        private void AnimateIngredientShowHide(List<Ingredient> ingredients){
+            
+        }
+
+        /// <summary>
+        /// ingredientsに含まれる素材を大砲に入れるアニメーション
+        /// </summary>
+        /// <param name="ingredients"></param>
+        private void AnimateCannonSetting(List<Ingredient> ingredients)
+        {
+            
         }
     }
 }
