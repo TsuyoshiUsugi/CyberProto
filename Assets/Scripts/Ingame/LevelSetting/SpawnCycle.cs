40b88e5065d54e512ee96a068f2eaf8f8c56b01a
nakamura <nkc.daichi.nakamura@gmail.com>
2023-08-04 16:25:42 +0900

    SpawnCycleのロールバックを修正

--- Assets/Scripts/Ingame/LevelSetting/SpawnCycle.cs
+++ Assets/Scripts/Ingame/LevelSetting/SpawnCycle.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public struct SpawnCycle
+namespace Game
 {
-    public float span;
-    public GameObject customerPrefab;
+    public struct SpawnCycle
+    {
+        public float span;
+        public Customer customerPrefab;
+    }
 }
