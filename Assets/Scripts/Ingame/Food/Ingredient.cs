ba73d813c9793b4d660fa3fde82d511034ae6a48
TsuyosiUsugi <qesulive@gmail.com>
2023-08-04 11:20:21 +0900

    [add] FoodとIngredientの中身追加

--- Assets/Scripts/Ingame/Food/Ingredient.cs
+++ Assets/Scripts/Ingame/Food/Ingredient.cs
@@ -1,18 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ingredient : MonoBehaviour
+[CreateAssetMenu(fileName = "Ingredient")]
+public class Ingredient : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public string Name;
+    public Sprite Icon;
+    public int Id;
 }
