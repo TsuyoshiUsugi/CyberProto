c96c902a4be2ff550e3de0ec5b82cbe714e22d12
tamaron <49875900+tamaron@users.noreply.github.com>
2023-08-04 11:23:27 +0900

    cannonの一時的変更

---
+++ Assets/Scripts/Ingame/Canon/Cannon.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Game
+{
+    public class Cannon : MonoBehaviour
+    {
+        private List<ingredient> _Selecting;
+        public List<Ingredient> Selecting { get; set; }
+    }
+
+    public class ingredient
+    {
+        
+    }
+    
+    public class Food
+    {
+        
+    }
+}
+
+
