82a5df57711298706eb9eefb521534ef606d473c
TsuyosiUsugi <qesulive@gmail.com>
2023-08-04 15:34:03 +0900

    [add] ステージセレクトのスクリプト追加

--- Assets/Scripts/StageSelect/AvairableStages.cs
+++ Assets/Scripts/StageSelect/AvairableStages.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AvairableStages : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+namespace StageSelect
+{
+    public class AvairableStages : MonoBehaviour
     {
-        
+        [SerializeField] LevelSettings[] _levelSettings;
+        public LevelSettings[] LevelSettings => _levelSettings;
     }
 }
