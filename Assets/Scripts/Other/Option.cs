319748fe955fc407c3faaef006d6ea1a4648d4b6
TsuyosiUsugi <qesulive@gmail.com>
2023-08-05 10:28:07 +0900

    [update] Option更新

--- Assets/Scripts/Other/Option.cs
+++ Assets/Scripts/Other/Option.cs
@@ -9,7 +9,28 @@ public class Option : ScriptableObject
     //•Û‘¶‚·‚é’l‚ª‚±‚êˆÈã‘‚¦‚é‚È‚ç\‘¢‘Ì‚É‚·‚é
     float _seValue;
     float _bgmValue;
-    public Subject<float> VolumeChanged = new Subject<float>();
+    public float SEValue
+    {
+        get => _seValue;
+        set
+        {
+            _seValue = value;
+            SeVolumeChanged.OnNext(value);
+        }
+    }
+    
+    public float BGMValue
+    {
+        get => _bgmValue;
+        set
+        {
+            _bgmValue = value;
+            BgmVolumeChanged.OnNext(value);
+        }
+    }
+
+    public Subject<float> SeVolumeChanged = new Subject<float>();
+    public Subject<float> BgmVolumeChanged = new Subject<float>();
     ISaveService _saveService;
     readonly string _seVolumeKey = "";
     readonly string _bgmVolumeKey = "";
